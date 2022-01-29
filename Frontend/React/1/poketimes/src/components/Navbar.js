c52d0c9e45b7d721917f48ec9121ef92edcf3b6a
Sid1608 <akarsiddharth@gmail.com>
2022-01-29 23:56:57 +0530

    learned about links and navlinks

--- Frontend/React/1/poketimes/src/components/Navbar.js
+++ Frontend/React/1/poketimes/src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react'
-
+import {Link,NavLink} from 'react-router-dom'
 const Navbar = () => {
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
                 <a href="#" className="brand-logo">Poke'Times</a>
                 <ul className="right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </div>
         </nav>
