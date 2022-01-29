183b8c08430129449e312ac500d23d9496147f2c
Sid1608 <akarsiddharth@gmail.com>
2022-01-29 11:54:15 +0530

    container and ui-components(stateless components)

--- Frontend/React/1/myapp/src/Ninjas.js
+++ Frontend/React/1/myapp/src/Ninjas.js
@@ -1,30 +1,20 @@
+import React, { Component } from "react";
 
-import React,{Component} from 'react'
-
-
-class Ninjas extends Component{
-  render() {
-    console.log(this.props);
-    //destructuring
-    // const {name,age,belt}=this.props;
-    const {ninjas}=this.props.ninjas;
-    const ninjaList=this.props.ninjas.map(ninja=>{
-        return (
-            <div className="ninja" key={ninja.id}>
-            
-                <div>Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div>Belt: {ninja.belt}</div>
-            </div>
-        )
-    })
+const Ninjas = (props) => {
+  console.log(props);
+  //destructuring
+  // const {name,age,belt}=this.props;
+  const { ninjas } = props.ninjas;
+  const ninjaList = ninjas.map((ninja) => {
     return (
-      <div className="ninja-list">
-        {ninjaList}
+      <div className="ninja" key={ninja.id}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+        <div>Belt: {ninja.belt}</div>
       </div>
     );
-  }
-  
-}
+  });
+  return <div className="ninja-list">{ninjaList}</div>;
+};
 
 export default Ninjas;
