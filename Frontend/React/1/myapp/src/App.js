b486dacfb3caa9f5c13afa8a7cf79d5405469129
Sid1608 <akarsiddharth@gmail.com>
2022-01-29 09:37:25 +0530

    Initialized react app using create-react-app

---
+++ Frontend/React/1/myapp/src/App.js
@@ -0,0 +1,17 @@
+
+import React,{Component} from 'react'
+import './App.css';
+
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <h1>My First React App</h1>
+        <p>Welcome</p>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
