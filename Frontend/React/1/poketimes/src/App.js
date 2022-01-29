b117e5961074eea821f3cef32466554a5b6152e5
Sid1608 <akarsiddharth@gmail.com>
2022-01-29 23:39:21 +0530

    added rotues using react router

--- Frontend/React/1/poketimes/src/App.js
+++ Frontend/React/1/poketimes/src/App.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react'
 import './App.css';
-
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Contact from './components/Contact';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <BrowserRouter>
         
-      </div>
+          <div className="App">
+              <Navbar/>
+              <Routes>
+                  <Route exact path='/' element={<Home/>}/>
+                  <Route path='/about' element={<About/>}/>
+                  <Route path='/contact' element={<Contact/>}/>
+              </Routes>
+          </div>
+        
+      </BrowserRouter>
+      
     )
   }
   
