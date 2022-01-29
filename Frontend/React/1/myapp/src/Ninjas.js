b321e0c96d77b5f1b8be50a5b7453c30ce46f877
Sid1608 <akarsiddharth@gmail.com>
2022-01-29 12:05:30 +0530

    conditional rendering: if,ternary

--- Frontend/React/1/myapp/src/Ninjas.js
+++ Frontend/React/1/myapp/src/Ninjas.js
@@ -1,20 +1,45 @@
 import React, { Component } from "react";
 
-const Ninjas = (props) => {
-  console.log(props);
+const Ninjas = ({ ninjas }) => {
+  // console.log(props);
   //destructuring
   // const {name,age,belt}=this.props;
-  const { ninjas } = props.ninjas;
+  // const { ninjas } = props.ninjas;
+  // const ninjaList = ninjas.map((ninja) => {
+  //   if(ninja.age>20){
+  //     return (
+  //       <div className="ninja" key={ninja.id}>
+  //         <div>Name: {ninja.name}</div>
+  //         <div>Age: {ninja.age}</div>
+  //         <div>Belt: {ninja.belt}</div>
+  //       </div>
+  //     );
+  //     }else {
+  //       return null;
+  //     }
+  // });
   const ninjaList = ninjas.map((ninja) => {
-    return (
+    return ninja.age > 20 ? (
       <div className="ninja" key={ninja.id}>
         <div>Name: {ninja.name}</div>
         <div>Age: {ninja.age}</div>
         <div>Belt: {ninja.belt}</div>
       </div>
-    );
+    ) : null;
   });
-  return <div className="ninja-list">{ninjaList}</div>;
+  return (
+    <div className="ninja-list">
+      {ninjas.map((ninja) => {
+        return ninja.age > 20 ? (
+          <div className="ninja" key={ninja.id}>
+            <div>Name: {ninja.name}</div>
+            <div>Age: {ninja.age}</div>
+            <div>Belt: {ninja.belt}</div>
+          </div>
+        ) : null;
+      })}
+    </div>
+  );
 };
 
 export default Ninjas;
