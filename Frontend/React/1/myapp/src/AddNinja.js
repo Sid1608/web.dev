85c750cafd0bb3264ee3c65820db24e26649c002
Sid1608 <akarsiddharth@gmail.com>
2022-01-29 13:21:55 +0530

    Passing function as props and adding data to state

--- Frontend/React/1/myapp/src/AddNinja.js
+++ Frontend/React/1/myapp/src/AddNinja.js
@@ -16,6 +16,7 @@ class AddNinja extends Component {
     handleSubmit=(e)=>{
         e.preventDefault();
         console.log(this.state);
+        this.props.addNinja(this.state);
 
     }
     render(){
--- Frontend/React/1/myapp/src/App.js
+++ Frontend/React/1/myapp/src/App.js
@@ -12,13 +12,28 @@ class App extends Component{
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
+  addNinja=(ninja)=>{
+    ninja.id=Math.random();
+    //here ... is spread operator what it is doign is taking array and spreading it out into its individual objects and it's putting those objects in this new array...
+    // let ninjas=[...this.state.ninjas] this array is equal to above array
+    let ninjas=[...this.state.ninjas,ninja];
+    this.setState({
+      ninjas: ninjas
+    });
+
+    //we can directly edit it without using setState it is bad to alter state without setState
+    // this.setState({
+    //   ninjas:this.ninjas.push(ninja)
+    // })
+    console.log(ninja);
+  }
   render() {
     return (
       <div className="App">
         <h1>My First React App</h1>
         <p>Welcome:)</p>
         <Ninjas ninjas={this.state.ninjas}/>
-        <AddNinja/>
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
