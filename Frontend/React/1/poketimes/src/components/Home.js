09d68de502634e8aafd5aa7287f9d3220af9de91
Sid1608 <akarsiddharth@gmail.com>
2022-01-30 17:47:44 +0530

    Mapping state to props

--- Frontend/React/1/poketimes/src/components/Home.js
+++ Frontend/React/1/poketimes/src/components/Home.js
@@ -2,23 +2,26 @@ import React,{Component} from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
 import PokeBall from "../pokeball.png"
+import {connect} from 'react-redux'
 class Home extends Component {
-    state={
-        posts:[]
-    }
-    componentDidMount() {
-        // this is async i.e it takes some time to go and do. it might take half second or more
-        //this returns promise(this action will complete in some point in time)
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(res=>{
-                console.log(res);
-                this.setState({
-                    posts:res.data.slice(0,10)
-                })
-            })
-    }
+    // state={
+    //     posts:[]
+    // }
+    // componentDidMount() {
+    //     this is async i.e it takes some time to go and do. it might take half second or more
+    //     this returns promise(this action will complete in some point in time)
+    //     axios.get('https://jsonplaceholder.typicode.com/posts')
+    //         .then(res=>{
+    //             console.log(res);
+    //             this.setState({
+    //                 posts:res.data.slice(0,10)
+    //             })
+    //         })
+    // }
     render() {
-        const {posts}=this.state;
+        console.log(this.props)
+        // const {posts}=this.state;
+        const {posts}=this.props;
         const postList=posts.length?(
             posts.map(post=>{
                 return(
@@ -47,4 +50,14 @@ class Home extends Component {
    
 }
 
-export default Home
+//we will pass it to connect function so that when we connect to redux 
+//it knows what data we want to grab from redux
+const mapStateToProps = (state)=>{
+    return{
+        posts:state.posts
+    }
+}
+
+// export default Home
+//connecting component to redux store
+export default connect(mapStateToProps)(Home)
