f4f61913ca012acfbe24ffc880ccc3413695640a
Sid1608 <akarsiddharth@gmail.com>
2022-01-30 17:21:26 +0530

    Setting up Redux in poketimes

--- Frontend/React/1/poketimes/src/index.js
+++ Frontend/React/1/poketimes/src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './reducers/rootReducer';
 
+const store=createStore(rootReducer);
 
+//we will pass our store into the provider and that is going to provide our application with the store so that the store can then interact with it
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
