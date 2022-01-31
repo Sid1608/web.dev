cb500fcae33aead068e36b1f9923e497e013e3b1
Sid1608 <akarsiddharth@gmail.com>
2022-01-31 19:40:15 +0530

    display booklist, remove books

--- Frontend/React/React Context And Hooks/booklist/src/components/BookList.js
+++ Frontend/React/React Context And Hooks/booklist/src/components/BookList.js
@@ -1,11 +1,22 @@
 import React, {useContext } from 'react';
-
+import { BookContext } from '../contexts/BookContext';
+import BookDetails from './BookDetails'
 
 const BookList = () => {
-    
-    return (
-        <div>
-            
+    const {books}=useContext(BookContext);
+    return books.length?(
+        <div className="book-list">
+            <ul>
+                {
+                    books.map(book=>{
+                        return <BookDetails book={book} key={book.id}/>
+                    })
+                }
+            </ul>
+        </div>
+    ):(
+        <div className="empty">
+            No books to read. Hello Free time :)
         </div>
     )
 }
