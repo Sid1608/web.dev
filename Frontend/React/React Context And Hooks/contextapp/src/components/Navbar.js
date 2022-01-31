44ad2f2d7439005d0199d34a768bc66a79573935
Sid1608 <akarsiddharth@gmail.com>
2022-01-31 12:02:23 +0530

    Accessing Context - Method 2

--- Frontend/React/React Context And Hooks/contextapp/src/components/Navbar.js
+++ Frontend/React/React Context And Hooks/contextapp/src/components/Navbar.js
@@ -1,13 +1,15 @@
-import React,{Component} from 'react'
-import { ThemeContext } from '../contexts/ThemeContext';
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
-    static contextType=ThemeContext;
-    render() { 
-        console.log(this.context);
-        const {isLightTheme,light,dark}=this.context;
-        const theme=isLightTheme? light:dark;
-        return (
+  //Method:1
+  // static contextType=ThemeContext;
+  render() {
+    /* console.log(this.context);
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+
             <nav style={{background:theme.ui,color:theme.syntax}}>
                 <h1>Context App</h1>
                 <ul>
@@ -16,8 +18,24 @@ class Navbar extends Component {
                     <li>Contact</li>
                 </ul>
             </nav>
-        )
-    }
+        )*/
+    return (
+      <ThemeContext.Consumer>{(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}</ThemeContext.Consumer>
+    );
+  }
 }
- 
+
 export default Navbar;
