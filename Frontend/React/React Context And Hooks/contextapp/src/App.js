c4769f81500a04e99c009924f7347fa8d7f71446
Sid1608 <akarsiddharth@gmail.com>
2022-01-31 12:42:22 +0530

    Creating Multiple Contexts

--- Frontend/React/React Context And Hooks/contextapp/src/App.js
+++ Frontend/React/React Context And Hooks/contextapp/src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import BookList from './components/BookList';
-import Navbar from './components/Navbar'
-import ThemeToggle from './components/ThemeToggle';
-import ThemeContextProvider from './contexts/ThemeContext'
+import React from "react";
+import BookList from "./components/BookList";
+import Navbar from "./components/Navbar";
+import ThemeToggle from "./components/ThemeToggle";
+import ThemeContextProvider from "./contexts/ThemeContext";
+import AuthContextProvider from "./contexts/AuthContext";
 function App() {
   return (
     <div className="App">
-    <ThemeContextProvider>
-      <Navbar/>
-      <BookList/>
-      <ThemeToggle/>
-    </ThemeContextProvider>
-    
+      <ThemeContextProvider>
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
+      </ThemeContextProvider>
+      {/* <AuthContextProvider>
+        <ThemeContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </ThemeContextProvider>
+      </AuthContextProvider> */}
     </div>
   );
 }
