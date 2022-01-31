67755ef89eb948e8441287a31e3ef974d747acf0
Sid1608 <akarsiddharth@gmail.com>
2022-01-31 13:16:36 +0530

    setup a basic hooksapp

---
+++ Frontend/React/React Context And Hooks/hooksapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SongList from './components/SongList'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <SongList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/React/React Context And Hooks/hooksapp/src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SongList = () => {
+    return (
+        <div className="song-list">
+            <ul>
+                <li>this wild darkness</li>
+                <li>memory gospel</li>
+            </ul>
+            
+        </div>
+    )
+}
+
+export default SongList
