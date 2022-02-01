b2dace3cbc9215ee3f568c8c1b931c0c82ef3a57
Sid1608 <akarsiddharth@gmail.com>
2022-02-01 11:17:52 +0530

    Added localStorage

--- Frontend/React/React Context And Hooks/booklist/src/contexts/BookContext.js
+++ Frontend/React/React Context And Hooks/booklist/src/contexts/BookContext.js
@@ -1,4 +1,4 @@
-import React, { useState ,createContext,useReducer} from 'react'
+import React, { useState ,createContext,useReducer,useEffect} from 'react'
 import {v4 as uuidv4} from 'uuid'
 import { bookReducer } from '../reducers/bookReducer';
 export const BookContext=createContext();
@@ -25,7 +25,15 @@ export const BookContext=createContext();
  
 export default BookContextProvider;*/
 const BookContextProvider = (props) => {
-    const [books, dispatch] = useReducer(bookReducer,[]);
+    const [books, dispatch] = useReducer(bookReducer,[],()=>{
+        const localData=localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : [];
+    });
+
+    //this will run on first render and then when books changes
+    useEffect(()=>{
+        localStorage.setItem('books',JSON.stringify(books));
+    },[books]);
     return (  
         <BookContext.Provider value={{books,dispatch}}>
             {props.children}
