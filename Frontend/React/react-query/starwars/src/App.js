039a2a6e84ac85bf0d4f5e123c7c265b183e0b9d
Sid1608 <akarsiddharth@gmail.com>
2022-02-01 19:40:53 +0530

    React Query Dev Tools

--- Frontend/React/react-query/starwars/src/App.js
+++ Frontend/React/react-query/starwars/src/App.js
@@ -2,20 +2,21 @@ import React,{useState} from 'react'
 import Navbar from './components/Navbar';
 import Planets from './components/Planets';
 import People from './components/People';
-
+import { ReactQueryDevtools } from 'react-query/devtools';
 function App() {
   const [page,setPage]=useState('planets');
 
   return (
-    <div className="App">
+    <>
+      <div className="App">
       <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage}/>
-      <div className="content">
-        {
-          page==='planets' ? <Planets/> :<People/>
-        }
+        <Navbar setPage={setPage} />
+        <div className="content">
+          { page === 'planets' ? <Planets /> : <People /> }
+        </div>
       </div>
-    </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
   );
 }
 
