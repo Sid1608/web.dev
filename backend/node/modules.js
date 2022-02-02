75f127fc2770a5908aa664d78e99b036dcf8c7fc
Sid1608 <akarsiddharth@gmail.com>
2022-02-02 12:07:49 +0530

    Node: Modules and require

---
+++ backend/node/modules.js
@@ -0,0 +1,16 @@
+// const xyz=require('./people')
+
+//destructuring
+const{people,ages}=require('./people')
+// console.log(people,ages)
+
+const os=require('os')
+console.log(os.platform(),os.homedir())
+
+// console.log(xyz);
+/**
+ {
+  people: [ 'yoshi', 'ryu', 'chun-li', 'mario' ],
+  ages: [ 20, 25, 22, 22, 22 ]
+}
+ */
---
+++ backend/node/people.js
@@ -0,0 +1,8 @@
+const people=['yoshi','ryu','chun-li','mario'];
+const ages=[20,25,22,22,22];
+console.log(people);
+
+//exporting single thing
+// module.exports=people;
+//exporting Multiple things
+module.exports={people,ages};
