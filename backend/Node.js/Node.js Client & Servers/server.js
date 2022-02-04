faf652662d73c12024364043d141990480f85cd3
Sid1608 <akarsiddharth@gmail.com>
2022-02-04 11:39:26 +0530

    Basic Routing

--- backend/Node.js/Node.js Client & Servers/server.js
+++ backend/Node.js/Node.js Client & Servers/server.js
@@ -16,8 +16,23 @@ const server=http.createServer((req,res)=>{
         //ending response: it sends response to the browser
         res.end();
     */
+
+
+    let path='./views/';
+    switch(req.url){
+        case '/':
+            path+='index.html';
+            break;
+        case '/about':
+            path+='about.html';
+            break;
+        default:
+            path+='404.html';
+            break;
+    }
+
    //send an html file 
-   fs.readFile('./views/index.html',(err,data)=>{
+   fs.readFile(path,(err,data)=>{
        if(err){
            console.log(err);
            res.end();
