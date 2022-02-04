edb152bb49913afae5a67585c3fa320c627e820b
Sid1608 <akarsiddharth@gmail.com>
2022-02-04 11:21:33 +0530

    learned about request and response object

--- backend/Node.js/Node.js Client & Servers/server.js
+++ backend/Node.js/Node.js Client & Servers/server.js
@@ -1,13 +1,18 @@
 const http = require('http');
-
-
 //storing instance of servers
 //callback function will fire every time the request comes to server
 const server=http.createServer((req,res)=>{
     //not logs in browser:runningon server not on browser
-    console.log('request made');
+    console.log(req.url,req.method);
+    //set header content type 
+    res.setHeader('Content-Type','text/html');
+    //write content to the res object
+    res.write('<head><link rel="stylesheet" href="#"></head>');
+    res.write('<p>hello ninjas<p>');
+    res.write('<p>hello again Ninjas<p>');
+    //ending response: it sends response to the browser
+    res.end();
 });
-
 //port, hostname, function
 server.listen(8000,'localhost',()=>{
     console.log('listening for request on port 8000')
