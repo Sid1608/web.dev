54499265251c42e32d9a84bffbadfafd1610527e
Sid1608 <akarsiddharth@gmail.com>
2022-02-04 11:31:37 +0530

    learned how to send html page to browser

--- backend/Node.js/Node.js Client & Servers/server.js
+++ backend/Node.js/Node.js Client & Servers/server.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+const { isBuffer } = require('util');
 //storing instance of servers
 //callback function will fire every time the request comes to server
 const server=http.createServer((req,res)=>{
@@ -6,12 +8,25 @@ const server=http.createServer((req,res)=>{
     console.log(req.url,req.method);
     //set header content type 
     res.setHeader('Content-Type','text/html');
-    //write content to the res object
-    res.write('<head><link rel="stylesheet" href="#"></head>');
-    res.write('<p>hello ninjas<p>');
-    res.write('<p>hello again Ninjas<p>');
-    //ending response: it sends response to the browser
-    res.end();
+    /* 
+        //write content to the res object
+        res.write('<head><link rel="stylesheet" href="#"></head>');
+        res.write('<p>hello ninjas<p>');
+        res.write('<p>hello again Ninjas<p>');
+        //ending response: it sends response to the browser
+        res.end();
+    */
+   //send an html file 
+   fs.readFile('./views/index.html',(err,data)=>{
+       if(err){
+           console.log(err);
+           res.end();
+       }else{
+           //res.write(data);
+           //    or
+           res.end(data);
+       }
+   })
 });
 //port, hostname, function
 server.listen(8000,'localhost',()=>{
