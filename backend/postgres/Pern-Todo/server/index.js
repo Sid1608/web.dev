c1ab732ec4589fdb021e5cc7684fc5d4fbfaa8d8
Sid1608 <akarsiddharth@gmail.com>
2022-02-19 16:35:25 +0530

    connected database and server

--- backend/postgres/Pern-Todo/server/index.js
+++ backend/postgres/Pern-Todo/server/index.js
@@ -1,9 +1,19 @@
 const express=require('express');
 const app=express();
 const cors=require('cors');
+const pool=require('./db')
 //middleware 
 app.use(cors());
 app.use(express.json());
+
+
+//ROUTES//
+//Create a todo
+//get all Todos
+//get a todo
+//updtate a todo
+//delete a todo
+
 
 app.listen(5000,()=>{
     console.log('server started on port 5000');
