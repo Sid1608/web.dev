53e40c9c611a7f7eeae9f35a27166d97c37c8042
Sid1608 <akarsiddharth@gmail.com>
2022-03-20 15:57:31 +0530

    learned about pages, routes, components

---
+++ Next.js/nextjs_net_ninja/ninjalist/comps/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            Copyright 2021 Ninja List
+        </div>
+    )
+}
+
+export default Footer
--- Next.js/nextjs_net_ninja/ninjalist/pages/index.js
+++ Next.js/nextjs_net_ninja/ninjalist/pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div >
+      <Navbar/>
       <h1>Home Page</h1>
+      <Footer/>
     </div>
   )
 }
