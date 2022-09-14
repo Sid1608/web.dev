f02947b077962f60b893c8303e271074e18cae48
sid1608 <akarsiddharth@gmail.com>
2022-09-14 19:20:13 +0530

    tutorial 10 and 9

---
+++ Golang/golang tutorial/Tutorial #10 - Multiple Return Values/main.go
@@ -0,0 +1,29 @@
+package main
+
+import(
+	"fmt"
+	"strings"
+)
+
+func getInitials(n string)(string, string){
+	s:=strings.ToUpper(n)
+	names:=strings.Split(s," ")
+
+	var initials []string
+	for _,v:=range names{
+		initials = append(initials,v[:1])//[0,1)
+	}
+
+	if len(initials)>1{
+		return initials[0],initials[1]
+	}
+	return initials[0],"_"
+}
+
+func main(){
+	fn1,sn1:=getInitials("tifa lockhart")
+	fmt.Println(fn1,sn1)
+	fn2,sn2:=getInitials("cloud strife")
+	fmt.Println(fn2,sn2)
+
+}
