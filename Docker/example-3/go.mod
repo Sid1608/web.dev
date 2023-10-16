60cad84b6b073d98d2fbeba69c7b91cdc0cee447
Siddharth Akar <akarsiddharth@gmail.com>
2023-10-16 20:51:19 +0530

    added docker example 3

---
+++ Docker/example-3/go.mod
@@ -0,0 +1,3 @@
+module docker-example-3
+
+go 1.19
---
+++ Docker/example-3/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello docker")
+}
+
+
+
+//go mod init docker-example-3
+//go build .
+//./docker-example-3
+//hello docker
